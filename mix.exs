b3e2cf77513a30f0b12eaa8e276017200fc73419
bolek <bolek@tolemi.com>
2020-12-08 09:59:24 -0500

    Bump version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExSeeClickFix.MixProject do
   def project do
     [
       app: :ex_see_click_fix,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps()
